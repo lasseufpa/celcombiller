606859366f56757df24afc23fa9f9b7e90f18b75
IteloFilho <itelofilho@gmail.com>
2015-11-13 11:09:34 -0300

    branch tmp to use imsi

--- models.py
+++ models.py
@@ -27,6 +27,7 @@ class User(db.Model):
     balance     = db.Column(db.Float, default=0)
     admin       = db.Column(db.Boolean)
     tunel       = db.relationship('CreditsRegister', secondary=tunel_table)
+    imsi        = db.Column(db.Integer, unique=True)
 
     def __init__(self , username ,password, clid, balance, admin):
         self.username   = username
